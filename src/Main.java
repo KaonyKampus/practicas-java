1a415364d83d93d12748a34561816b35462065c1
David Silva <davidsilvav6@gmail.com>
2024-09-18 20:52:43 -0500

    segundo_commit_prueba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero1 = 35;
+        int numero2 = 48;
+
+        int suma = numero1 + numero2;
+
+        System.out.println("suma");
     }
 }
